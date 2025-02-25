d6055f04643b4c3b3d32b660f0ef6d2f1cd58303
klxcoder <klxcoder@gmail.com>
2025-02-26 06:53:55 +0700

    Refactor message receiving logic to use async/await and improve error handling

--- app/receive.js
+++ app/receive.js
@@ -1,28 +1,31 @@
 #!/usr/bin/env node
 
-const amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
 
-amqp.connect('amqp://my-rabbit-server', (error0, connection) => {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel((error1, channel) => {
-    if (error1) {
-      throw error1;
-    }
+const receiveMessages = async () => {
+  try {
+    const connection = await amqp.connect('amqp://my-rabbit-server');
+    const channel = await connection.createChannel();
 
     const queue = 'hello';
 
-    channel.assertQueue(queue, {
+    await channel.assertQueue(queue, {
       durable: false
     });
 
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-    channel.consume(queue, (msg) => {
+    channel.consume(queue, async (msg) => {
       console.log(" [x] Received %s", msg.content.toString());
+      // Example of an async operation
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      console.log(" [x] Done processing %s", msg.content.toString());
     }, {
       noAck: true
     });
-  });
-});
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+receiveMessages();
