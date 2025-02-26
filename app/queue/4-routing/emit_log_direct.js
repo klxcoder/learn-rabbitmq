54cdffec7d3ddda1b6f654e2abf9b8a2a10768f3
klxcoder <klxcoder@gmail.com>
2025-02-26 19:16:11 +0700

    refractor emit_log_direct.js to use const and arrow function

--- app/queue/4-routing/emit_log_direct.js
+++ app/queue/4-routing/emit_log_direct.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://my-rabbit-server', function (error0, connection) {
+amqp.connect('amqp://my-rabbit-server', (error0, connection) => {
   if (error0) {
     throw error0;
   }
-  connection.createChannel(function (error1, channel) {
+  connection.createChannel((error1, channel) => {
     if (error1) {
       throw error1;
     }
-    var exchange = 'direct_logs';
-    var args = process.argv.slice(2);
-    var msg = args.slice(1).join(' ') || 'Hello World!';
-    var severity = (args.length > 0) ? args[0] : 'info';
+    const exchange = 'direct_logs';
+    const args = process.argv.slice(2);
+    const msg = args.slice(1).join(' ') || 'Hello World!';
+    const severity = (args.length > 0) ? args[0] : 'info';
 
     channel.assertExchange(exchange, 'direct', {
       durable: false
     });
     channel.publish(exchange, severity, Buffer.from(msg));
-    console.log(" [x] Sent %s: '%s'", severity, msg);
+    console.log(` [x] Sent ${severity}: '${msg}'`);
   });
 
-  setTimeout(function () {
+  setTimeout(() => {
     connection.close();
     process.exit(0);
   }, 500);
