12bb1c80ce14ef86344a55c0d391356744e06140
klxcoder <klxcoder@gmail.com>
2025-02-26 21:06:38 +0700

    refractor rpc_server.js and rpc_client.js
    + use const intead of var
    + use arrow function
    + use async/await

--- app/queue/6-rpc/rpc_client.js
+++ app/queue/6-rpc/rpc_client.js
@@ -1,56 +1,54 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
 
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 if (args.length === 0) {
   console.log("Usage: rpc_client.js num");
   process.exit(1);
 }
 
-amqp.connect('amqp://my-rabbit-server', function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1;
-    }
-    channel.assertQueue('', {
+async function main() {
+  try {
+    const connection = await amqp.connect('amqp://my-rabbit-server');
+    const channel = await connection.createChannel();
+
+    const correlationId = await generateUuid();
+    const num = parseInt(args[0]);
+
+    console.log(' [x] Requesting fib(%d)', num);
+
+    const q = await channel.assertQueue('', {
       exclusive: true
-    }, function (error2, q) {
-      if (error2) {
-        throw error2;
-      }
-      var correlationId = generateUuid();
-      var num = parseInt(args[0]);
-
-      console.log(' [x] Requesting fib(%d)', num);
-
-      channel.consume(q.queue, function (msg) {
-        if (msg.properties.correlationId === correlationId) {
-          console.log(' [.] Got %s', msg.content.toString());
-          setTimeout(function () {
-            connection.close();
-            process.exit(0);
-          }, 500);
-        }
-      }, {
-        noAck: true
-      });
-
-      channel.sendToQueue('rpc_queue',
-        Buffer.from(num.toString()), {
-        correlationId: correlationId,
-        replyTo: q.queue
-      });
     });
-  });
-});
 
-function generateUuid() {
+    channel.consume(q.queue, (msg) => {
+      if (msg.properties.correlationId === correlationId) {
+        console.log(' [.] Got %s', msg.content.toString());
+        setTimeout(() => {
+          connection.close();
+          process.exit(0);
+        }, 500);
+      }
+    }, {
+      noAck: true
+    });
+
+    await channel.sendToQueue('rpc_queue',
+      Buffer.from(num.toString()), {
+      correlationId: correlationId,
+      replyTo: q.queue
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function generateUuid() {
   return Math.random().toString() +
     Math.random().toString() +
     Math.random().toString();
 }
+
+main();
--- app/queue/6-rpc/rpc_server.js
+++ app/queue/6-rpc/rpc_server.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
 
-amqp.connect('amqp://my-rabbit-server', function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1;
-    }
-    var queue = 'rpc_queue';
+async function main() {
+  try {
+    const connection = await amqp.connect('amqp://my-rabbit-server');
+    const channel = await connection.createChannel();
 
-    channel.assertQueue(queue, {
+    const queue = 'rpc_queue';
+
+    await channel.assertQueue(queue, {
       durable: false
     });
+
     channel.prefetch(1);
+
     console.log(' [x] Awaiting RPC requests');
-    channel.consume(queue, function reply(msg) {
-      var n = parseInt(msg.content.toString());
+
+    channel.consume(queue, (msg) => {
+      const n = parseInt(msg.content.toString());
 
       console.log(" [.] fib(%d)", n);
 
-      var r = fibonacci(n);
+      const r = fibonacci(n);
 
       channel.sendToQueue(msg.properties.replyTo,
         Buffer.from(r.toString()), {
@@ -31,8 +31,10 @@ amqp.connect('amqp://my-rabbit-server', function (error0, connection) {
 
       channel.ack(msg);
     });
-  });
-});
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function fibonacci(n) {
   if (n === 0 || n === 1)
@@ -40,3 +42,5 @@ function fibonacci(n) {
   else
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+main();
