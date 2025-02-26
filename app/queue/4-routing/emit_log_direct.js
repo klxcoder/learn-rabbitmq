7b9ea19334e1dc2b9c277ca0ee8316a27a1c419d
klxcoder <klxcoder@gmail.com>
2025-02-26 19:19:05 +0700

    refractor emit_log_direct.js.js for using async and await

--- app/queue/4-routing/emit_log_direct.js
+++ app/queue/4-routing/emit_log_direct.js
@@ -1,29 +1,30 @@
 #!/usr/bin/env node
 
-const amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
 
-amqp.connect('amqp://my-rabbit-server', (error0, connection) => {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel((error1, channel) => {
-    if (error1) {
-      throw error1;
-    }
+async function emitLogDirect() {
+  try {
+    const connection = await amqp.connect('amqp://my-rabbit-server');
+    const channel = await connection.createChannel();
+
     const exchange = 'direct_logs';
     const args = process.argv.slice(2);
     const msg = args.slice(1).join(' ') || 'Hello World!';
     const severity = (args.length > 0) ? args[0] : 'info';
 
-    channel.assertExchange(exchange, 'direct', {
+    await channel.assertExchange(exchange, 'direct', {
       durable: false
     });
     channel.publish(exchange, severity, Buffer.from(msg));
     console.log(` [x] Sent ${severity}: '${msg}'`);
-  });
 
-  setTimeout(() => {
-    connection.close();
-    process.exit(0);
-  }, 500);
-});
+    setTimeout(() => {
+      connection.close();
+      process.exit(0);
+    }, 500);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+emitLogDirect();
