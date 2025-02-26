1b4031add1438e7a388216ada8ebdfb1408a8955
klxcoder <klxcoder@gmail.com>
2025-02-26 07:00:22 +0700

    Refactor message sending logic to use async/await and improve error handling

--- app/send.js
+++ app/send.js
@@ -1,28 +1,29 @@
 #!/usr/bin/env node
 
-const amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
 
-amqp.connect('amqp://my-rabbit-server', (error0, connection) => {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel((error1, channel) => {
-    if (error1) {
-      throw error1;
-    }
+const sendMessage = async () => {
+  try {
+    const connection = await amqp.connect('amqp://my-rabbit-server');
+    const channel = await connection.createChannel();
 
     const queue = 'hello';
     const msg = 'Hello World!';
 
-    channel.assertQueue(queue, {
+    await channel.assertQueue(queue, {
       durable: false
     });
     channel.sendToQueue(queue, Buffer.from(msg));
 
     console.log(" [x] Sent %s", msg);
-  });
-  setTimeout(() => {
-    connection.close();
-    process.exit(0);
-  }, 500);
-});
+
+    setTimeout(() => {
+      connection.close();
+      process.exit(0);
+    }, 500);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+sendMessage();
